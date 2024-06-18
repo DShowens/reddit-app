ea9701ddf53e47c9a51524e7b1d866eac7892112
DShowens <donshowens@gmail.com>
2024-06-17 20:49:56 -0400

    updated index.js to use React Router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="#" element={<Placeholder />} />  */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
